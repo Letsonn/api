a634d7378c2678ebc24e0b48d70d151fbc903e8a
Matheus Felicio <matheus.point998@gmail.com>
2021-08-26 23:20:18 -0300

    delete employment service

--- src/modules/employments/routes/employments.routes.ts
+++ src/modules/employments/routes/employments.routes.ts
@@ -9,5 +9,6 @@ const employmentsController = new EmploymentsController();
 employmentsRouter.get('/:employmentId', isAuthenticated, employmentsController.listOne);
 employmentsRouter.post('/', isAuthenticated, employmentsController.create);
 employmentsRouter.patch('/:employmentId', isAuthenticated, employmentsController.update);
+employmentsRouter.delete('/:employmentId', isAuthenticated, employmentsController.delete);
 
 export default employmentsRouter;
